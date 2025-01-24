d0620a5b59ec1db04e5cc85c9109f45899fba01d
Richard Essuman <ressuman001@gmail.com>
2025-01-24 23:09:36 +0000

    `feat: Initial setup of Node.js project with Express, Mongoose, and dependencies`
    set up Express server with middleware; implement admin model with validation

---
+++ index.js
@@ -0,0 +1,51 @@
+// Packages
+//import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+// Utilities
+import connectDB from "./config/db.js";
+import IndexError from "./middlewares/indexError.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+dotenv.config();
+const port = process.env.PORT_NUMBER || 4200;
+
+// Database Connection
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(helmet()); // Secure HTTP headers
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend URL
+    credentials: true, // Allow cookies
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+); // Configure CORS
+
+app.use(morgan("dev")); // Logging requests
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(cookieParser());
+
+// Error Handling Middleware
+app.all("*", (err, req, res) => {
+  next(new IndexError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
+
+// Start Server
+app.listen(port, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port: ${port}, and listening to requests at http://localhost:${port}`
+  );
+});
---
+++ models/users/adminModel.js
@@ -0,0 +1,102 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const adminSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      minlength: [2, "Username must be at least 2 characters long"],
+      maxlength: [30, "Username must be at most 30 characters long"],
+      index: true,
+    },
+
+    last_name: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+      minlength: [2, "Username must be at least 2 characters long"],
+      maxlength: [30, "Username must be at most 30 characters long"],
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validator: [validator.isEmail, "Please enter a valid email address"],
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+
+    passwordConfirm: {
+      type: String,
+      required: [true, "Password confirm is required"],
+      minlength: [6, "Password confirm must be at least 6 characters long"],
+      select: false,
+      validate: {
+        // This only works on CREATE and SAVE!!!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords are not the same",
+      },
+    },
+
+    contact: {
+      type: String,
+      required: [true, "Contact is required"],
+      trim: true,
+      minlength: [6, "Contact must be attempting 6 characters long"],
+      maxlength: [15, "Contact must be at most 15 characters long"],
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    OTP: {
+      type: String,
+      default: null,
+      select: false,
+    },
+
+    OTPExpires: {
+      type: Date,
+      default: null,
+      select: false,
+    },
+
+    resetPasswordOTP: {
+      type: String,
+      default: null,
+      select: false,
+    },
+
+    resetPasswordOTPExpires: {
+      type: Date,
+      default: null,
+      select: false,
+    },
+
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+export default Admin;
